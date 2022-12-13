bdb5ab7a8b4c57b0f1921fe3168515dee1acb2fb
Brian Duff <cairndubh@gmail.com>
2022-12-12 18:36:57 -0800

    Make the button do something

--- src/main-page.ts
+++ src/main-page.ts
@@ -9,11 +9,19 @@ export class MainPage extends LitElement {
   @property()
   version = 'STARTING';
 
+  @property()
+  message = "Click the button, please.";
+
   render() {
     return html`
-    Welcome to a simple Google Material 3 page!
+    Welcome to a simple Google Material 3 page! ${this.message}
     <md-filled-text-field label="Name"></md-filled-text-field>
-    <md-filled-button label="Hello world"></md-filled-button>
+    <md-filled-button @click="${this.clickButton}" label="Hello world"></md-filled-button>
     `;
+  }
+
+  private clickButton(e: Event) {
+    console.log("Button clicked")
+    this.message = "You clicked! Whey hey!"
   }
 }
